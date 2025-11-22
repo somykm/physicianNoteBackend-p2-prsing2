f1b7b8fcded57c23541c013498f40487abdd184d
https://github.com/somykm <sk43@hood.edu>
2025-11-21 19:30:47 -0500

    Added @NoArgsConstructor:to generate a constructor with no parameters  and @AllArgsConstructor:to generate a constructor with no parameters to ignore the error I got due to them

--- src/main/java/com/abernathyclinic/medilabo_physicianNotes/model/PatientNoteView.java
+++ src/main/java/com/abernathyclinic/medilabo_physicianNotes/model/PatientNoteView.java
@@ -1,12 +1,17 @@
 package com.abernathyclinic.medilabo_physicianNotes.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientNoteView {
         private Integer patId;
         private String fullName;
         private List<String> notes;
+
 }
